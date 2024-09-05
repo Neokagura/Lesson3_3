b6598e57b8ccc2852f801c01c6b5ddd171abdbbd
Ness <bur>
2024-09-05 18:29:58 +0300

    Добавьте файлы проекта.

---
+++ Lesson3_3/Lesson3_3.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+   
+    int x; 
+    cout << "Введите число: ";
+    cin >> x;
+    cout << "-----Проверяем-----\n";
+    if ( x % 2 == 0) {
+        cout << "Число " << x << " - четное\n";
+    }
+    else {
+        cout << "Число " << x << " - нечетное";
+
+    }
+}
+
